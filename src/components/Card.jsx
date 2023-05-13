d8746e65de7eb8abb4e22522380096746d3f501b
donvine mugendi <donvinemugendi@gmail.com>
2023-05-13 03:48:19 +0300

    updated styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card'> 
-    <h1>Simon Carter Green Shirt</h1>
-      <img 
-        src="https://imagescdn.simoncarter.in/img/app/product/4/404834-2469768.jpg?auto=format"
-        alt="my  picture"
-        width="200px"
-        height="300px"
-      />
-     
-      <h2>Description</h2>
-      <p>
-        The Simon Carter Green Shirt is a stylish and versatile wardrobe staple
-        that effortlessly combines classic design with contemporary flair. Made
-        with high-quality materials and meticulous craftsmanship, this shirt
-        exudes sophistication and refinement. The shirt features a rich green
-        color that adds a touch of vibrancy and uniqueness to any outfit. The
-        premium fabric ensures comfort and durability, allowing you to wear it
-        with confidence for various occasions.
-      </p>
+      <img src={props.image} alt="product image" />
+      <div className='description'>
+    <h1>{props.title}</h1>
+    <h2>Description</h2>
+      <p>{props.description}</p>
+    </div>
     </div>
   );
 }
+Card.defaultProps ={description: `The Simon Carter Green Shirt is a stylish and versatile wardrobe staple
+that effortlessly combines classic design with contemporary flair. Made
+with high-quality materials and meticulous craftsmanship, this shirt
+exudes sophistication and refinement.`,
+title:`Simon Carter Green Shirt`,
+image:`https://imagescdn.simoncarter.in/img/app/product/4/404834-2469768.jpg?auto=format`}
 
 export default Card
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-
-
-
 const Form = () => {
   return (
     <form id="form">
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" placeholder='Enter Email' required />
+      <h2>Review This Product</h2>
+      <label htmlFor="email">Name</label>
+      <input type="text" id="name" placeholder='Enter Your Name' required />
       <br />
       <br />
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" placeholder='********' required />
+      <label htmlFor="review">Review</label>
+      <textarea type="review" id="review" placeholder='Type your review here' required />
       <br />
       <br />
       <button type="submit" className="btn">
