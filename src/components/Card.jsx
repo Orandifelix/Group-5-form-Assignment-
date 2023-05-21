cc768ea2b0d8cf2c4934e3a187e791bebce90ac7
donvine mugendi <donvinemugendi@gmail.com>
2023-05-21 22:14:35 +0300

    updated form reviews

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types';
 
-const Card = (props) => {
+
+const Card = ({image, title, description}) => {
   return (
     <div className='card'> 
-      <img src={props.image} alt="product image" />
+      <img src={image} alt="product image" />
       <div className='description'>
-    <h1>{props.title}</h1>
+    <h1>{title}</h1>
+    <h2> KSH 1,599 <span>Ksh 2,500</span> <strong>-36%</strong></h2>
+    <small>Few Units Left</small>
+    <p id="ratings"><span>★★★★☆</span>(106 ratings)</p>
     <h2>Description</h2>
-      <p>{props.description}</p>
+      <p>{description}</p>
     </div>
     </div>
   );
 }
-Card.defaultProps ={description: `The Simon Carter Green Shirt is a stylish and versatile wardrobe staple
-that effortlessly combines classic design with contemporary flair. Made
-with high-quality materials and meticulous craftsmanship, this shirt
-exudes sophistication and refinement.`,
-title:`Simon Carter Green Shirt`,
-image:`https://imagescdn.simoncarter.in/img/app/product/4/404834-2469768.jpg?auto=format`}
-Card.propTypes={title:PropTypes.string.isRequired, description:PropTypes.string.isRequired, image:PropTypes.string.isRequired}
 
 export default Card
